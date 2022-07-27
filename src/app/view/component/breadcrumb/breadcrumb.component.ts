b97f28240920e9d1164c4fcec7b5bcb24ca253b0
Huu An <81819172+catabada@users.noreply.github.com>
2022-07-27 19:40:50 +0700

    breadcrumb component

--- src/app/view/component/breadcrumb/breadcrumb.component.ts
+++ src/app/view/component/breadcrumb/breadcrumb.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-breadcrumb',
+  selector: 'breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
