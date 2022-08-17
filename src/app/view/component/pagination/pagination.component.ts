b32de067a4a7ece63d44c762874a343c943d3dce
Huu An <81819172+catabada@users.noreply.github.com>
2022-08-17 13:44:45 +0700

    fix conflict

--- src/app/view/component/pagination/pagination.component.ts
+++ src/app/view/component/pagination/pagination.component.ts
@@ -9,7 +9,9 @@ export class PaginationComponent implements OnInit {
   @Output() currentPage = new EventEmitter();
   numPage: number = 1;
   @Input() pagination: any;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
@@ -18,18 +20,21 @@ export class PaginationComponent implements OnInit {
     this.numPage = page;
     this.currentPage.emit(page);
   }
+
   nextPage() {
     if (this.numPage < this.pagination.totalPages()) {
       this.numPage++;
       this.currentPage.emit(this.numPage);
     }
   }
+
   previousPage() {
     if (this.numPage > 1) {
       this.numPage--;
       this.currentPage.emit(this.numPage);
     }
   }
+
   counter(i: number) {
     return Array(i);
   }
