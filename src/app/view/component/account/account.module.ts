6bc2050b19256af6146b81a37a69d0aec7e25f03
An <19130003@st.hcmuaf.edu.vn>
2022-08-20 07:40:54 +0700

    change account to component

--- src/app/view/component/account/account.module.ts
+++ src/app/view/component/account/account.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import {SharedModule} from "../../shared/shared.module";
 import {RouterModule} from "@angular/router";
 import {AccountComponent} from "./account.component";
-import {SidebarComponent} from "../../component/sidebar/sidebar.component";
+import {SidebarComponent} from "../sidebar/sidebar.component";
 import {MdbSidenavModule} from "mdb-angular-ui-kit/sidenav";
-import {AddressComponent} from "../../component/address/address.component";
-import {ProfileComponent} from "../../component/profile/profile.component";
+import {AddressComponent} from "../address/address.component";
+import {ProfileComponent} from "../profile/profile.component";
 import {MdbDatepickerModule} from "mdb-angular-ui-kit/datepicker";
 import {MdbRadioModule} from "mdb-angular-ui-kit/radio";
-import {OrderHistoryComponent} from "../../component/order-history/order-history.component";
+import {OrderHistoryComponent} from "../order-history/order-history.component";
 
 
 
